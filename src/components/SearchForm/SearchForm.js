3131d3965e3fc0b90fb7c40db45b9ef145cbfcb1
Sveta Apriamashvili <s.v.apriamashvili@yandex.com>
2021-10-05 11:13:07 +1100

    fix review comments

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,7 +7,7 @@ function SearchForm() {
         <section className="search-form">
             <form className="search-form__form">
                 <fieldset className="search-form__input-container">
-                    <input className="search-form__item" placeholder="Фильм" type="text" id="movie" name="movie">
+                    <input className="search-form__item" placeholder="Фильм" type="text" id="movie" name="movie" required>
                     </input>
                     <button className="search-form__submit-button" type="submit">Поиск</button>
                 </fieldset>
